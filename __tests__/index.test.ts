cd0c422346cfe0326568927e8d8430b115518d70
沈孟平 <shen990206@gmail.com>
2023-03-13 11:18:04 +0800

    test: add vitest

---
+++ __tests__/index.test.ts
@@ -0,0 +1,9 @@
+import { describe, test, it, expect } from 'vitest';
+
+describe('index', () => {
+  test('test', () => {
+    it('should work', () => {
+      expect(1).toBe(1);
+    });
+  });
+});
